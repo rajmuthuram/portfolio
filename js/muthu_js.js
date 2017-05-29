1c5b70d1bb0b8bc468b697517255076bf09d8a74
Navin Kumar <navin@auctionsoftware.com>
2017-05-29 10:47:53 +0530

    inital commit

---
+++ js/muthu_js.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+$(document).ready(function(){
+ 	
+	AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+	
+	
+});
+
